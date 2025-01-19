b7f4f756af69bc7c9675c602089e0a1b244f5369
Jaleed khan <132694164+JaleedKhanofficial@users.noreply.github.com>
2025-01-19 23:07:11 +0500

    Add files via upload

---
+++ Q27.py
@@ -0,0 +1,36 @@
+# Step 27
+# It's time to test the valid_in_row method. Call valid_in_row on gameboard. Pass it 0 and 8 as the arguments and print the result.
+
+# Again, note how the method is defined with three parameters, yet it is called with only two arguments because self is automatically passed as the object on which the method is called.
+
+
+class Board:
+    def __init__(self, board):
+        self.board = board
+
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+
+puzzle = [
+  [0, 0, 2, 0, 0, 8, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 7, 6, 2],
+  [4, 3, 0, 0, 0, 0, 8, 0, 0],
+  [0, 5, 0, 0, 3, 0, 0, 9, 0],
+  [0, 4, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 4, 6, 7, 0, 0, 0],
+  [0, 8, 6, 7, 0, 4, 0, 0, 0],
+  [0, 0, 0, 5, 1, 9, 0, 0, 8],
+  [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
+
+gameboard = Board(puzzle)
+print(gameboard.valid_in_row(0,8))          # step 27
---
+++ Q30.py
@@ -0,0 +1,35 @@
+# Step 30
+# Next, you're going to create a method that checks if a number can be inserted in a specified column of the sudoku board by checking if the number is not already present in that column.
+
+# Within the Board class, create a method named valid_in_col and give it three parameters: self, col and num. Where col and num are the column index and the number to be checked, respectively.
+
+class Board:
+    def __init__(self, board):
+        self.board = board
+
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+    def valid_in_col(self,col,num):                 # step 30
+        pass                                        # step 30
+puzzle = [
+  [0, 0, 2, 0, 0, 8, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 7, 6, 2],
+  [4, 3, 0, 0, 0, 0, 8, 0, 0],
+  [0, 5, 0, 0, 3, 0, 0, 9, 0],
+  [0, 4, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 4, 6, 7, 0, 0, 0],
+  [0, 8, 6, 7, 0, 4, 0, 0, 0],
+  [0, 0, 0, 5, 1, 9, 0, 0, 8],
+  [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
+
+gameboard = Board(puzzle)
