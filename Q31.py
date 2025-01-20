3ebb9ad944ec2aea0cc648f4563f88777d201718
Jaleed khan <132694164+JaleedKhanofficial@users.noreply.github.com>
2025-01-20 13:41:30 +0500

    Add files via upload

---
+++ Q31.py
@@ -0,0 +1,37 @@
+# Step 31
+# You need to check if a given number is not equal to the number in the specified column of the current row.
+
+# For this, replace pass with a generator expression that iterates over the range from 0 to 8 (inclusive), and for each row, evaluates whether the number at the specified row and column col on the board is different from num.
+
+
+class Board:
+    def __init__(self, board):
+        self.board = board
+
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+    def valid_in_col(self, col, num):
+        (self.board[row][col] != num for row in range(9))           # step 31
+        
+puzzle = [
+  [0, 0, 2, 0, 0, 8, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 7, 6, 2],
+  [4, 3, 0, 0, 0, 0, 8, 0, 0],
+  [0, 5, 0, 0, 3, 0, 0, 9, 0],
+  [0, 4, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 4, 6, 7, 0, 0, 0],
+  [0, 8, 6, 7, 0, 4, 0, 0, 0],
+  [0, 0, 0, 5, 1, 9, 0, 0, 8],
+  [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
+
+gameboard = Board(puzzle)
---
+++ Q36.py
@@ -0,0 +1,40 @@
+# Step 36
+# Another thing to check is if a number can be inserted in a 3x3 square.
+
+# Inside the Board class, create a method named valid_in_square with four parameters: self, row, col, and num. Where row, col, and num represent the row index, the column index, and the number to be checked, respectively.
+
+class Board:
+    def __init__(self, board):
+        self.board = board
+
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+
+    def valid_in_col(self, col, num):
+        return all(self.board[row][col] != num for row in range(9))
+    
+    def valid_in_square(self, row, col, num):           # step 36
+        pass                                            # step 36
+    
+puzzle = [
+  [0, 0, 2, 0, 0, 8, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 7, 6, 2],
+  [4, 3, 0, 0, 0, 0, 8, 0, 0],
+  [0, 5, 0, 0, 3, 0, 0, 9, 0],
+  [0, 4, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 4, 6, 7, 0, 0, 0],
+  [0, 8, 6, 7, 0, 4, 0, 0, 0],
+  [0, 0, 0, 5, 1, 9, 0, 0, 8],
+  [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
+
+gameboard = Board(puzzle)
